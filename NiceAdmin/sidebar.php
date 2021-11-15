187848217b594caf81f554d00f46142e257bc721
alvin <alvinarpit@zoyoecommerce.com>
2021-11-15 18:07:55 +0530

    all HTML and CSS files

---
+++ NiceAdmin/sidebar.php
@@ -0,0 +1,93 @@
+<ul class="sidebar-nav" id="sidebar-nav">
+
+  <li class="nav-item">
+    <a class="nav-link " href="index.html">
+      <i class="bi bi-grid"></i>
+      <span>Dashboard</span>
+    </a>
+  </li><!-- End Dashboard Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" data-bs-target="#charts-nav" data-bs-toggle="collapse" href="#">
+      <i class="fab fa-creative-commons-sampling-plus"></i><span>Create</span><i class="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul id="charts-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+      <li>
+        <a href="photoeditor.php">
+          <i class="bi bi-circle"></i><span>Photo Editor</span>
+        </a>
+      </li>
+      <li>
+        <a href="addgraphic.php">
+          <i class="bi bi-circle"></i><span>Add Graphic</span>
+        </a>
+      </li>
+      <li>
+        <a href="createpost.php">
+          <i class="bi bi-circle"></i><span>Create a Post</span>
+        </a>
+      </li>
+    </ul>
+  </li><!-- End Create Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+      <i class="far fa-paper-plane"></i><span>Publish</span><i class="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+      <li>
+        <a href="">
+          <i class="bi bi-circle"></i><span>Schedule a Post</span>
+        </a>
+      </li>
+      <li>
+        <a href="">
+          <i class="bi bi-circle"></i><span>Content Plan</span>
+        </a>
+      </li>
+      <li>
+        <a href="">
+          <i class="bi bi-circle"></i><span>CSV Import</span>
+        </a>
+      </li>
+    </ul>
+  </li><!-- End Components Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
+      <i class="fas fa-network-wired"></i><span>Administration</span><i class="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+      <li>
+        <a href="">
+          <i class="bi bi-circle"></i><span>Accounts</span>
+        </a>
+      </li>
+      <li>
+        <a href="forms-layouts.html">
+          <i class="bi bi-circle"></i><span>Roles & Projects</span>
+        </a>
+      </li>
+      <li>
+        <a href="forms-editors.html">
+          <i class="bi bi-circle"></i><span>Social Inbox</span>
+        </a>
+      </li>
+      <li>
+        <a href="forms-validation.html">
+          <i class="bi bi-circle"></i><span>Statistics</span>
+        </a>
+      </li>
+      <li>
+        <a href="forms-validation.html">
+          <i class="bi bi-circle"></i><span>Analytics</span>
+        </a>
+      </li>
+      <li>
+        <a href="forms-validation.html">
+          <i class="bi bi-circle"></i><span>Settings</span>
+        </a>
+      </li>
+    </ul>
+  </li><!-- End Forms Nav -->
+</ul>
